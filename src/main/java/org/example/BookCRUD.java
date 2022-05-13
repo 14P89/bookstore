cb134c006b958ac8fdcef1d0ce2e954178b9bbac
14P89 <przem14k@gmail.com>
2022-05-13 19:44:57 +0200

    after CRUD

--- src/main/java/org/example/BookCRUD.java
+++ src/main/java/org/example/BookCRUD.java
@@ -7,6 +7,9 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class BookCRUD {
 
@@ -41,11 +44,144 @@ public class BookCRUD {
         return id;
     }
 
+    public Book getBook (Integer id) {
+        Session session = null;
+        Book book = null;
+
+        try {
+            session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+
+            String hql = "FROM Book B WHERE B.id = " + id;
+            Query query = session.createQuery(hql);
+            List results = query.list();
+
+            if (results.size() > 0) {
+                book = (Book) results.get(0);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                session.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return book;
+    }
+
+    public List getAllBooks () {
+        Session session = null;
+        List results = null;
+
+        try {
+            session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+
+            String hql = "FROM Book B";
+            Query query = session.createQuery(hql);
+            results = query.list();
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                session.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return results;
+    }
+
+    public void updateBook (Integer id, int numberOfPages) {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+            Book book = (Book) session.get(Book.class,id);
+
+            book.setNumberOfPages(numberOfPages);
+
+            session.update(book);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            try {
+                session.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void deleteBook (Integer id) {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+            Book book = (Book) session.get(Book.class,id);
+
+            session.delete(book);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            try {
+                session.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+
+
+
     public void run() {
         Book book2 = new Book("Ogniem i Mieczem", "Henryk Sienkiewicz", 1921, 625);
 
-        Integer newID = saveBook(book2);
-        System.out.println(newID);
+        Book book1 = getBook(1);
+        System.out.println(book1);
+        System.out.println("--");
+        List allBooks = getAllBooks();
+        allBooks.forEach(System.out::println);
+
+        //updateBook(1,500);
+        System.out.println("\n");
+        allBooks.forEach(System.out::println);
+
+        System.out.println("Skasowanie ksiazki");
+        //deleteBook(2);
+
+        //allBooks.forEach(System.out::println);
+
     }
 
 
